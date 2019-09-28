67a837ed60642dd236f3d9a20f0edec1ae3b19e4
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-28 18:11:37 +0800

    Update 5.Image arithmetics and Logic.py

--- 5.Image arithmetics and Logic.py
+++ 5.Image arithmetics and Logic.py
@@ -1,1 +1,21 @@
+import cv2
+import numpy as np
 
+img1 = cv2.imread('3D-Matplotlib.png')
+img2 = cv2.imread('Main-SVM-Image.png')
+img3 = cv2.imread('Main-Logo.png')
+
+# 逻辑运算
+add1 = img1 + img2
+# 将全部的像素值相加在一起
+add2 = cv2.add(img1,img2)
+# 加权，0代表γ值
+weighted = cv2.addWeighted(img1,0.6,img2,0.4,0)
+
+rows,cols,channels = img3.shape
+roi = img[0:rows,0:cols]
+
+cv2.imshow('add',add)
+cv2.imshow('weighted',weighted)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
