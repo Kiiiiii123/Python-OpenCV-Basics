ee6164f100bfe4082de038962ede3616e2fab02f
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-28 16:27:13 +0800

    Update 2.Loading Video Source.py

--- 2.Loading Video Source.py
+++ 2.Loading Video Source.py
@@ -3,6 +3,8 @@ import numpy as np
 
 # 针对Web Camera输入或者是视频文件
 cap=cv2.VideoCapture(0) 
+fourcc=cv2.VideoWriter_fourcc(*'XVID')
+out=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
 
 while True:
   # rec返回True或者False
@@ -10,6 +12,7 @@ while True:
   
   # 转换灰色
   gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+  out.write(frame)
   
   # 出现两个窗口
   cv2.imshow('frame',frame)
@@ -19,4 +22,5 @@ while True:
     break
     
 cap.release()
+out.release()
 cv2.destroyAllWindows()
