18d708204c77cbb7b2fded25ae621968bd6224c3
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-28 16:34:54 +0800

    Update 3.Drawing and Writing on Image.py

--- 3.Drawing and Writing on Image.py
+++ 3.Drawing and Writing on Image.py
@@ -1,1 +1,11 @@
+import cv2
+import numpy as np
 
+img = cv2.imread('MyImage.jpg',cv2.IMREAD_COLOR)
+
+# 画直线，注意颜色是BGR
+cv2.line(img,(0,0),(150,150),(255,0,0),15)
+
+imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
