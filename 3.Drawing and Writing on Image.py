a7af852dd884ce8487457a9e36beee099b003465
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-09-28 16:58:22 +0800

    Update 3.Drawing and Writing on Image.py

--- 3.Drawing and Writing on Image.py
+++ 3.Drawing and Writing on Image.py
@@ -12,6 +12,16 @@ cv2.rectangle(img,(15,25),(200,150),(255,255,255),5)
 # 画圆形，-1表示填充
 cv2.circle(img,(100,63),55,(0,0,255),-1)
 
+# 画多段线
+pts=np.array([[10,5],[20,30],[70,20],[50,10]],np.int32)
+# pts=pts.reshape((-1,1,2))
+# True决定要不要将最后一个点与第一个点相连接形成闭环多边形
+cv2.polylines(img,[pts],True,(0,255,255),3)
+
+# 加文本，1表示字体大小，2表示字体粗细
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OpenCV Tuts!',(0,130),font,1,(200,250,250),2,cv2.LINE_AA)
+
 imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
