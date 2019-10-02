ae0788c599a9cf9fcc0202404bea6e699e7492d4
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-02 17:21:40 +0800

    Update 7.Color Filtering.py

--- 7.Color Filtering.py
+++ 7.Color Filtering.py
@@ -1,1 +1,9 @@
+import cv2 
+import numpy as np
 
+cap = cv2.VideoCapture(0)
+
+while True:
+  _,frame = cap.read()
+  # 转换至HSV颜色空间，H表示色彩[0,179]，S表示饱和度[0,255]，V表示亮度[0,255]
+  hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
