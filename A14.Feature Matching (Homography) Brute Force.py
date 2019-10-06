8c0f9e3ae29800ec6ce20efcdb52675c659b084d
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-07 00:55:20 +0800

    Update A14.Feature Matching (Homography) Brute Force.py

--- A14.Feature Matching (Homography) Brute Force.py
+++ A14.Feature Matching (Homography) Brute Force.py
@@ -1,5 +1,22 @@
 # 不同光照条件或者不同角度进行蛮力特征匹配 单应性矩阵Homography
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
+img1 = cv2.imread('opencv-feature-matching-template.jpg',0)
+img2 = cv2.imread('opencv-feature-matching-image.jpg',0)
 
+orb = cv2.ORB_create()
+
+kp1,des1 = orb.detectAndCompute(img1,None)
+kp2,des2 = orb.detectAndCompute(img2,None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck = True)
+
+matches = bf.match(des1,des2)
+matches = sorted(matches,key = lambda x:x.distance)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None,flags = 2)
+
+plt.imshow(img3)
+plt.show()
