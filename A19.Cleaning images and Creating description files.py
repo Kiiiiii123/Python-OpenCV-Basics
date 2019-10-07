e90ad6acf1964a771b16a3d6195efe3f87f3cced
ZhangChao <zhangchaoa@buaa.edu.cn>
2019-10-07 21:52:47 +0800

    Update A19.Cleaning images and Creating description files.py

--- A19.Cleaning images and Creating description files.py
+++ A19.Cleaning images and Creating description files.py
@@ -23,7 +23,39 @@ def store_raw_images():
     except Exception as e:
       print(str(e))
       
+# 存在链接中的图片不存在的情况  ，这里需要摆脱这些图片，新建一个uglies文件夹，复制其中一张图片到该文件夹中    
 def find_uglies():
-  
+  for file_type in ['neg']:  # 可以是'pos'
+    # 在neg文件夹下对图片进行遍历
+    for img in os.listdir(file_type):
+      # neg、pos文件夹下均存在ugly图片
+      for ugly in os.listdir('uglies'):
+        try:
+          current_image_path = str(file_type)+'/'+str(img)
+          ugly = cv2.imread('uglies/'+str(ugly))
+          question = cv2.imread(current_image_path)
+          
+          if ugly.shape == question.shape and not (np.bitwise_xor(ugly,question).any()):
+            print('Ugly!')
+            os.remove(current_image_path)
+            
+        escept EXception as e:
+          print(str(e))
 
-store_raw_images()
+# 创建描述文件          
+def create_pos_n_neg():
+  for file_type in ['neg']:
+    for img in os.listdir(file_type):
+      if file_type == 'neg':
+        line = file_type+'/'+img+'\n'
+        with open('bg.txt','a') as f:
+          f.write(line)
+      
+      elif file_type == 'pos':
+        line = file_type+'/'+img+'1 0 0 50 50\n'
+        with open('bg.txt','a') as f:
+          f.write(line)
+          
+create_pos_n_neg()          
+# find_uglies()
+# store_raw_images()
